f4be74db1c336c4d576b6851df194d68dd86788d
Potts <ianpottinger@gmail.com>
2020-01-09 23:52:20 +0000

    Manual setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Title of readme
+
+Some description
--- setup.py
+++ setup.py
@@ -35,19 +35,59 @@ KEYWORDS = keyword.kwlist
 from setuptools import setup
 import wheel
 
+
+with open("README.md", "r") as markdown:
+    overview = markdown.read()
+
+
 setup (name = 'Py3onic',
        version = '1.3.5.7.9',
        description = 'Py3onic code',
        author = 'Potts',
        author_email = 'Potts@ian.org',
+       url = "https://github.com/ianpottinger/Python3",
        packages = ['Py3onic'],
+       package_dir = {'': 'src'},
+       classifiers = [
+           "Environment :: Win32 (MS Windows)",
+           "Framework :: IDLE",
+           "Framework :: IPython",
+           "License :: Freeware",
+           "License :: Free For Home Use",
+           "License :: Free For Educational Use",
+           "License :: OSI Approved :: MIT License",
+           "License :: Public Domain",
+           "Natural Language :: English",
+           "Operating System :: Microsoft :: MS-DOS",
+           "Operating System :: Microsoft :: Windows :: Windows XP",
+           "Operating System :: Microsoft :: Windows :: Windows 7",
+           "Operating System :: Microsoft :: Windows :: Windows 10",
+           "Operating System :: OS Independent",
+           "Programming Language :: Pascal",
+           "Programming Language :: Python",
+           "Programming Language :: Python :: 3.8",
+           "Topic :: Software Development :: Libraries :: pygame"
+           ],
+       long_description = overview,
+       long_description_content_type = "text/markdown",
+       install_requires = [
+           "maths ~= 1.3",
+            ]       
        )
+
+
+"""
+https://pypi.org/classifiers/
+"""
+
 
 """
 python3 setup.py sdist
-python3 setup.py bdist_wheel
+python3 setup.py bdist_wheel sdist
 python3 setup.py register
 python3 setup.py sdist upload
+
+pip3 install -e .
 """
 
 
